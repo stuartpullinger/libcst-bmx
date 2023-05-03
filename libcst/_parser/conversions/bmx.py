08acf9e44163035efbb0bb222dc11f5b6dab1c30
Stuart Pullinger <stuart.pullinger@oddbox.co.uk>
2023-05-03 20:37:45 +0100

    wip: filling out BmxOpenClose Node

--- libcst/_parser/conversions/bmx.py
+++ libcst/_parser/conversions/bmx.py
@@ -44,42 +44,106 @@ from libcst._parser.whitespace_parser import parse_parenthesizable_whitespace
 
 @add_slots
 @dataclass(frozen=True)
+class BmxAttribute(CSTNode):
+    """
+    A BMX attribute node
+    """
+    key: SimpleString
+    value: typing.Optional[BaseExpression]
+
+    #: Whitespace after the key, but before the colon in ``key : value``.
+    whitespace_before_equals: BaseParenthesizableWhitespace = SimpleWhitespace.field("")
+    #: Whitespace after the colon, but before the value in ``key : value``.
+    whitespace_after_equals: BaseParenthesizableWhitespace = SimpleWhitespace.field("")
+
+    class KeywordArgsT(typing.TypedDict):
+        whitespace_before_equals: CSTNode
+        whitespace_after_equals: CSTNode
+        value: CSTNode
+
+    def _visit_and_replace_children(self, visitor: CSTVisitorT) -> "BmxAttribute":  
+        value_keyword = {}
+        if self.value is not None:
+            # Do I _really_ have to use a typed dict here to please the type checker?
+            value_keyword = self.KeywordArgsT(
+                whitespace_before_equals=visit_required(
+                    self, "whitespace_before_equals", self.whitespace_before_equals, visitor
+                ),
+                whitespace_after_equals=visit_required(
+                    self, "whitespace_after_equals", self.whitespace_after_equals, visitor
+                ),
+                value=visit_required(self, "value", self.value, visitor),
+            )
+
+        return BmxAttribute(
+            key=visit_required(self, "key", self.key, visitor),
+            **value_keyword
+        )
+
+    def _codegen_impl(self, state: CodegenState) -> None:
+        self.key._codegen(state)
+        if self.value is not None:
+            state.add_token('=')
+            self.key._codegen(state)
+
+@add_slots
+@dataclass(frozen=True)
 class BmxOpenClose(_BaseParenthesizedNode, CSTNode):
     """
     A BMX open-close node
     """
     
-    close_opentag: GreaterThan = GreaterThan.field()
+    ref: typing.Any
+    attributes: typing.Sequence[BmxAttribute]
     contents: typing.Sequence[BaseElement]
-    open_closetag: LessThanSlash = LessThanSlash.field()
-    close_ref: typing.Sequence[BaseElement]
-    close_closetag: typing.Sequence[BaseElement]
+    close_ref: typing.Any
     
+    open_opentag: LessThan = LessThan.field()
+    close_opentag: GreaterThan = GreaterThan.field()
+    open_closetag: LessThanSlash = LessThanSlash.field()
+    close_closetag: SlashGreaterThan = SlashGreaterThan.field()
 
     lpar: typing.Sequence[LeftParen] = ()
     #: Sequence of parenthesis for precedence dictation.
     rpar: typing.Sequence[RightParen] = ()
 
-    def _visit_and_replace_children(self, visitor: CSTVisitorT) -> "BmxFragment":
+    def _validate(self) -> None:
+        assert self.ref.deep_equals(self.close_ref) 
+
+    def _visit_and_replace_children(self, visitor: CSTVisitorT) -> "BmxOpenClose":
         return BmxOpenClose(
-            close_opentag=close_opentag,
-            contents=contents,
-            open_closetag=open_closetag,
-            close_ref=close_ref,
-            close_closetag=close_closetag,
+            open_opentag=visit_required(self, 'open_opentag', self.open_opentag, visitor),
+            ref=visit_required(self, 'ref', self.ref, visitor),
+            attributes=visit_sequence(self, 'attributes', self.attributes, visitor),
+            close_opentag=visit_required(self, 'close_opentag', self.close_opentag, visitor),
+            contents=visit_sequence(self, 'contents', self.contents, visitor),
+            open_closetag=visit_required(self, 'open_closetag', self.open_closetag, visitor),
+            close_ref=visit_required(self, 'close_ref', self.close_ref, visitor),
+            close_closetag=visit_required(self, 'close_closetag', self.close_closetag, visitor),
         )
 
     def _codegen_impl(self, state: CodegenState) -> None:
         with self._parenthesize(state):
+            self.open_opentag._codegen(state)
+            self.ref._codegen(state)
+            attributes = self.attributes
+            for idx, el in enumerate(attributes):
+                el._codegen(
+                    state,
+                    default_comma=(idx < len(attributes) - 1),
+                    default_comma_whitespace=True,
+                )
+            self.close_opentag._codegen(state)
             elements = self.contents
-            self.start_fragment._codegen(state)
             for idx, el in enumerate(elements):
                 el._codegen(
                     state,
                     default_comma=(idx < len(elements) - 1),
                     default_comma_whitespace=True,
                 )
-            self.end_fragment._codegen(state)
+            self.open_closetag._codegen(state)
+            self.close_ref._codegen(state)
+            self.close_closetag._codegen(state)
 
 @add_slots
 @dataclass(frozen=True)
@@ -110,7 +174,7 @@ class BmxSelfClosing(_BaseParenthesizedNode, CSTNode):
     A BMX self-closing node
     """
     ref: typing.Any
-    attributes: typing.Sequence[DictElement]
+    attributes: typing.Sequence[BmxAttribute]
     
     opener: LessThan = LessThan.field()
     closer: EndSelfClosing = EndSelfClosing.field()
@@ -189,50 +253,6 @@ def convert_bmx_selfclosing(
 @with_production("bmx_tag", "'<' dotted_name bmx_attribute*")
 def convert_bmx_tag(config: ParserConfig, children: typing.Sequence[typing.Any]) -> typing.Any:
     return children
-
-@add_slots
-@dataclass(frozen=True)
-class BmxAttribute(CSTNode):
-    """
-    A BMX attribute node
-    """
-    key: SimpleString
-    value: typing.Optional[BaseExpression]
-
-    #: Whitespace after the key, but before the colon in ``key : value``.
-    whitespace_before_equals: BaseParenthesizableWhitespace = SimpleWhitespace.field("")
-    #: Whitespace after the colon, but before the value in ``key : value``.
-    whitespace_after_equals: BaseParenthesizableWhitespace = SimpleWhitespace.field("")
-
-    class KeywordArgsT(typing.TypedDict):
-        whitespace_before_equals: CSTNode
-        whitespace_after_equals: CSTNode
-        value: CSTNode
-
-    def _visit_and_replace_children(self, visitor: CSTVisitorT) -> "BmxAttribute":  
-        value_keyword = {}
-        if self.value is not None:
-            # Do I _really_ have to use a typed dict here to please the type checker?
-            value_keyword = self.KeywordArgsT(
-                whitespace_before_equals=visit_required(
-                    self, "whitespace_before_equals", self.whitespace_before_equals, visitor
-                ),
-                whitespace_after_equals=visit_required(
-                    self, "whitespace_after_equals", self.whitespace_after_equals, visitor
-                ),
-                value=visit_required(self, "value", self.value, visitor),
-            )
-
-        return BmxAttribute(
-            key=visit_required(self, "key", self.key, visitor),
-            **value_keyword
-        )
-
-    def _codegen_impl(self, state: CodegenState) -> None:
-        self.key._codegen(state)
-        if self.value is not None:
-            state.add_token('=')
-            self.key._codegen(state)
 
 # bmx_attribute: (NAME | atom_string) ['=' atom]
 # Allow attribute keys to be python keywords eg. 'class', etc
